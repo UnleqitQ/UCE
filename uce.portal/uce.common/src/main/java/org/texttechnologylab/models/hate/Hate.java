a0f330c0f212f0e72c3a3d85f18146551f78cef6
mirelamuza <s7375418@stud.uni-frankfurt.de>
2025-07-09 19:37:15 +0200

    Add hate and non-hate properties to Hate entity with corresponding getters and setters

--- uce.portal/uce.common/src/main/java/org/texttechnologylab/models/hate/Hate.java
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/models/hate/Hate.java
@@ -9,6 +9,12 @@ import javax.persistence.*;
 @Entity
 @Table(name = "hate")
 public class Hate extends UIMAAnnotation implements WikiModel {
+    @Column(name = "hate", nullable = false)
+    private double hate;
+
+    @Column (name = "non_hate", nullable = false)
+    private double nonHate;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "document_id", nullable = false)
     private Document document;
@@ -24,6 +30,22 @@ public class Hate extends UIMAAnnotation implements WikiModel {
     public Hate(int begin, int end, String coveredText) {
         super(begin, end);
         setCoveredText(coveredText);
+    }
+
+    public double getHate() {
+        return hate;
+    }
+
+    public  void setHate(double hate) {
+        this.hate = hate;
+    }
+
+    public double getNonHate() {
+        return nonHate;
+    }
+
+    public void setNonHate(double nonHate) {
+        this.nonHate = nonHate;
     }
 
     public Document getDocument() {
