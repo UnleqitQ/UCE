74afa810d94c352720114227ffcf60a743905b60
UnleqitQ <quentin.weinbrenner@gmail.com>
2026-06-18 17:30:59 +0200

    feat: add inspector support

--- uce.portal/uce.common/src/main/java/org/texttechnologylab/uce/common/services/LexiconService.java
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/uce/common/services/LexiconService.java
@@ -43,7 +43,8 @@ public class LexiconService {
                     Cue.class,
                     Scope.class,
                     XScope.class,
-                    UnifiedTopic.class));
+                    UnifiedTopic.class,
+                    RecognizedTaxon.class));
 
     public LexiconService(PostgresqlDataInterface_Impl db) {
         this.db = db;
